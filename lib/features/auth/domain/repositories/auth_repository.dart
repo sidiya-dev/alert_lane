a6b1e040ddbf29d395e9d1eab0283b1967bbb586
sidiya-dev <sidiya@email.com>
2026-03-04 23:30:24 +0000

    user repository

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,7 @@
+import 'package:alert_lane/core/errors/failure.dart';
+import 'package:alert_lane/features/auth/domain/entities/user_entity.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure,UserEntity>> login();
+}
